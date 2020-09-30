1326f5e74bc80f8955aba589f7a0aeb345161fb7
Chris Palmer <cgpalmer91@gmail.com>
2020-09-30 21:19:05 +0000

    Adding a comment to make sure the code function is clear.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -82,6 +82,7 @@ SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# These are settings for the signup requirements.
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
