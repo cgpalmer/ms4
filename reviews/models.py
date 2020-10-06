da8b6839d8b3729cd6484e39adf9005e23ba3812
Chris Palmer <cgpalmer91@gmail.com>
2020-10-06 22:46:55 +0000

    Creating the reviews model and uploading the json file. Registered it too.

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+class Review(models.Model):
+    product = models.ForeignKey('products.Product', null=True, blank=True, on_delete=models.SET_NULL)
+    review_rating = models.IntegerField(null=False, blank=False)
+    review_content = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.review_rating
+
+    def get_review_content(self):
+        return self.review_content
