1cfafd4e427a4019bf39c0410b971b4f57d710c9
Chris Palmer <cgpalmer91@gmail.com>
2020-10-06 21:32:36 +0000

    Re-uploading a simple version of the products for now and making sure the models reflect it. Removing extra models as I need to build it up in layers.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+    def get_content(self):
+        return self.content
+    
--- products/models.py
+++ products/models.py
@@ -12,42 +12,19 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Special(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
-
-
-
-
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    blog = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    special_offer = models.ForeignKey('Special', null=True, blank=True, on_delete=models.SET_NULL)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True)
     image_url_mobile = models.URLField(max_length=1024, null=True, blank=True)
     image_url_desktop = models.URLField(max_length=1024, null=True, blank=True)
     image_mobile = models.ImageField(null=True, blank=True)
     image_desktop = models.ImageField(max_length=1024, null=True, blank=True)
-    blog = models.ForeignKey('Blog', null=True, blank=True, on_delete=models.SET_NULL)
-
+  
     def __str__(self):
         return self.name
 
-
-class Reviews(models.Model):
-    sku = models.CharField(max_length=254)
-    username = models.CharField(max_length=254)
-    review = models.TextField()
-    rating = models.IntegerField()
-
-    def __str__(self):
-        return self.name
