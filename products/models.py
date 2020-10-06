af657c7fa4ea04969324d573d7bbdc33fa979650
Chris Palmer <cgpalmer91@gmail.com>
2020-10-06 22:10:37 +0000

    Adding in the special model, registering it, making a special offer column in Product and then making it a foreign key.

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from blog.models import Blog
 
 
 class Category(models.Model):
@@ -15,11 +16,12 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    blog = models.CharField(max_length=254, null=True, blank=True)
+    blog = models.ForeignKey('blog.Blog', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.IntegerField(null=True, blank=True)
+    special_offer = models.ForeignKey('Special', null=True, blank=True, on_delete=models.SET_NULL)
     image_url_mobile = models.URLField(max_length=1024, null=True, blank=True)
     image_url_desktop = models.URLField(max_length=1024, null=True, blank=True)
     image_mobile = models.ImageField(null=True, blank=True)
@@ -28,3 +30,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+
+class Special(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
