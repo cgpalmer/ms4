bfe18ddc18374d610a1a9fd81c63af049107605f
Chris Palmer <cgpalmer91@gmail.com>
2020-10-07 22:27:33 +0000

    Adding in the product and blog urls to hiddenGems url patterns.

--- hiddenGems/urls.py
+++ hiddenGems/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
+    path('blog/', include('blog.urls')),
 ]
