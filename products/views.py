c9090c8a79d1114f1b2267076c506875ea5f127b
Chris Palmer <cgpalmer91@gmail.com>
2020-10-07 22:26:45 +0000

    Creating a view for the blog and making sure it has a accessible url. Creating the product.html and start to pull through some of the data from the models.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+''' A view to show all the products and somewhere to search and sort by.'''
+
+def all_products(request):
+    # Returning the index page
+
+    products = Product.objects.all
+
+    context = {
+        'products': products
+    }
+    return render(request, 'products/products.html', context)
