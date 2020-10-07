c99d2700ace958718a52cef303c2e802a61e17a1
Chris Palmer <cgpalmer91@gmail.com>
2020-10-07 21:25:12 +0000

    Creating an admin class for all of the models in products.

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,32 @@ from django.contrib import admin
 from .models import Product, Category, Special
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Special)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image_mobile',
+        'special_offer'
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class SpecialAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'discounts',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Special, SpecialAdmin)
