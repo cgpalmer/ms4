ebcf0cde31826eef7046ec39f9490dab8254cc01
Chris Palmer <cgpalmer91@gmail.com>
2020-10-07 22:25:46 +0000

    Creating a view for the blog and making sure it has a accessible url in urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+''' A view to show the blog associated with the product.'''
+
+
+def blog_info(request):
+    # Returning the index page
+
+    blogs = Blog.objects.all
+
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog/blog.html', context)
