c4dc5fd95a68eb1ea436849e12d1b574ca388876
Chris Palmer <cgpalmer91@gmail.com>
2020-10-07 21:38:45 +0000

    Creating admin class for blog and showing the title.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Blog
 # Register your models here.
-admin.site.register(Blog)
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+admin.site.register(Blog, BlogAdmin)
