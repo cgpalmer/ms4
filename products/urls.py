01b9507dfa8e54678b25f8238ee507ff3ce3dd7a
Chris Palmer <cgpalmer91@gmail.com>
2020-10-08 22:48:47 +0000

    Creating the view and url for the new product detail page.

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # The path is empty so it goes to the root home page.
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 ''' A view to show all the products and somewhere to search and sort by.'''
 
 def all_products(request):
-    # Returning the index page
+    # Returning the products page
 
-    products = Product.objects.all
+    products = Product.objects.all()
 
     context = {
-        'products': products
+        'products': products,
     }
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    # Returning the details of a specific product.
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
