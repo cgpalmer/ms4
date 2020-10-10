73ae5fe6d7b35d08131197eeab09969b12648bff
Chris Palmer <cgpalmer91@gmail.com>
2020-10-10 09:27:20 +0000

    Creating a view and url to handle the form form the product details page. Update the html to have the correct url to post form too.

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,19 @@ from django.shortcuts import render
 def view_basket(request):
     # Returning the user's basket
     return render(request, 'basket/basket.html')
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the chosen product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
