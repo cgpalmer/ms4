4a6722a50650c3593ae5da1e33b9fd109f314e94
Chris Palmer <cgpalmer91@gmail.com>
2020-10-10 17:41:08 +0000

    Creating a template for the checkout form, installing crispy forms and ensuring it is set up to be used on every page. Also re-wrote the requirements.txt after installing crispy form.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'reviews',
     'basket',
     'checkout'
+
+    # External apps
+    'crispy-forms',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hiddenGems.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'basket.context.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
