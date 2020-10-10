2f7f3b58f448cdc3b160b54f696093fb4d5d2743
Chris Palmer <cgpalmer91@gmail.com>
2020-10-10 10:10:27 +0000

    Using the session variable to pull user items into the context.py. Iterating through the items and making it accessible to the rest of the pages. Including a bug fix for converting delivery to decimal rather than float and adding in the html template to render the total on the nav bar.

--- basket/context.py
+++ basket/context.py
@@ -1,14 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_AMOUNT:
-        delivery = settings.STANDARD_DELIVERY_AMOUNT
+        delivery = Decimal.from_float(settings.STANDARD_DELIVERY_AMOUNT)
         free_delivery_deficit = settings.FREE_DELIVERY_AMOUNT - total
     else:
         delivery = 0
