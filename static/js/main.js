dde5ea2cd91a8aa9e1d42975a8cec290b893a465
Chris Palmer <cgpalmer91@gmail.com>
2020-10-11 23:38:22 +0000

    Adding in the css and js to get the links to line up and hide/unhide when hovered on in desktop and getting the search bar to appear in mobile.

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,15 @@ $( document ).ready(function() {
     $("#searchProducts").click(function(){
         $("#extendedSearchBar").toggle();
     });
+
+    $("#desktopNavAllSearch").click(function(){
+        $("#desktopExtendedSearchBar").toggle();
+    });
+
+     $("#desktopExtraNavWrapper").hover(function(){
+        $("#desktopDropDownMenu").css('display', 'flex');}
+        ,function(){
+    $("#desktopDropDownMenu").css('display', 'none');
+    });
+
 });
