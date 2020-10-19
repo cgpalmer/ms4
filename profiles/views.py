1687d99f7d2dcf95000da9dd5bb17ab974b2f446
Chris Palmer <cgpalmer91@gmail.com>
2020-10-19 20:27:46 +0000

    Putting the username into the context for being displayed on the user_profile.

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 def profile(request):
     """ Display the user's profile. """
-
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profiles.html'
-    context = {}
+    context = {
+        'current_user': profile,
+    }
 
     return render(request, template, context)
