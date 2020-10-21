621ca1ef45f889608e51348cad3c4d0fee32d0b1
Chris Palmer <cgpalmer91@gmail.com>
2020-10-21 21:07:04 +0000

    Organising the secret key to be in the environ.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '12=i7xi5(iku%xfomiktdx+qwn=_9e=4qezc84@xkih*vwdykv'
+SECRET_KEY = os.get.environ('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
