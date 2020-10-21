db72b984c0271fa104a8c2ca1cede19830d60cc2
Chris Palmer <cgpalmer91@gmail.com>
2020-10-21 17:45:17 +0000

    Adding in the template for the user add a product to the admin.

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 # The path is empty so it goes to the root home page.
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,10 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
+from .models import Product, Category
+from .forms import ProductForm
+
+
 # Create your views here.
 ''' A view to show all the products and somewhere to search and sort by.'''
 
@@ -67,3 +71,24 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
