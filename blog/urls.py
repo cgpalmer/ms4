b4270b7201572bc186a15b257696c4cbefe99d7e
Chris Palmer <cgpalmer91@gmail.com>
2020-10-23 22:55:16 +0000

    Creating a blog detail view and adding links to the blog page to see individual blogs.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # The path is empty so it goes to the root home page.
 urlpatterns = [
-    path('', views.blog_info, name='blogs')
+    path('', views.blog_info, name='blogs'),
+    path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 ''' A view to show the blog associated with the product.'''
@@ -14,3 +14,14 @@ def blog_info(request):
         'blogs': blogs,
     }
     return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+    return render(request, 'blog/blog_detail.html', context)
+
+
