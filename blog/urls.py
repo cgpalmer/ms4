ebfb021b97ed995b5beb3d03fc45c3cfa79614db
Chris Palmer <cgpalmer91@gmail.com>
2020-10-23 23:18:30 +0000

    Providing a link from the product detail to a specific blog post, making a seperate view and configuring the urls.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 # The path is empty so it goes to the root home page.
 urlpatterns = [
     path('', views.blog_info, name='blogs'),
-    path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail')
+    path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'),
+    path('blog/<int:product_id>/', views.blog_detail_from_product, name='blog_detail_from_product')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from products.models import Product
 # Create your views here.
 ''' A view to show the blog associated with the product.'''
 
@@ -24,4 +25,11 @@ def blog_detail(request, blog_id):
     }
     return render(request, 'blog/blog_detail.html', context)
 
+def blog_detail_from_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    blog = get_object_or_404(Blog, sku=product.sku)
 
+    context = {
+        'blog': blog,
+    }
+    return render(request, 'blog/blog_detail.html', context)
