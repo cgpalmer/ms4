4877bb6ec890e206774a7e5033731444f2572b22
Chris Palmer <cgpalmer91@gmail.com>
2020-10-29 13:45:09 +0000

    Reworking the form so the review rating had a minimum attribute of 0

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,9 +3,8 @@ from .models import Review
 
 
 class ReviewForm(forms.ModelForm):
-
     class Meta:
         model = Review
         fields = '__all__'
 
-
+    review_rating = forms.IntegerField(label='Rating', widget=forms.NumberInput(attrs={'min': 0}))
