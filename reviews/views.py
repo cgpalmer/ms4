f9aef9a5b19c0dd50adccd4febe7008b2b9d6659
Chris Palmer <cgpalmer91@gmail.com>
2020-10-29 11:42:08 +0000

    putting the django message template in the base.html to ensure it renders properly.

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.shortcuts import render, reverse, redirect
 from .forms import ReviewForm
 from django.contrib import messages
 
 # Create your views here.
+
 
 def add_review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             review = form.save()
+            messages.success(request, 'Review added.')
+            print("Review added.")
             return redirect(reverse('products'))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')  
+            messages.error(request, "'Failed to add product.")
             print("you've gone past the error messages.")
             return redirect(reverse('add_review'))
     else:
