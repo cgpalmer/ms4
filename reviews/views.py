5ac5f239c105f6b24680ae4dec17a80ebbaacc41
Chris Palmer <cgpalmer91@gmail.com>
2020-10-30 18:28:57 +0000

    When a review is added the total amount of reviews is increased by one and the new score is added to the running rating. Average is then found to display on the html.

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .forms import ReviewForm
 from .models import Review
+from products.models import Product
 from django.contrib import messages
 
 # Create your views here.
@@ -11,6 +12,39 @@ def add_review(request):
         form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             review = form.save()
+
+            # retrieve data from current product
+            product_name = form.cleaned_data.get("product")
+            print(product_name)
+            retrieve_product = get_object_or_404(Product, name=product_name)
+            print(retrieve_product.name)
+            number_of_ratings = retrieve_product.number_of_ratings
+            ratings_total = retrieve_product.rating_total
+            print(ratings_total)
+
+            # retrieving data from the form 
+            new_rating_score = form.cleaned_data.get("review_rating")
+            print("new rating score")
+            print(new_rating_score)
+
+            # Average rating calculations
+            # Adding up the total score of all the reviews
+            retrieve_product.rating_total = ratings_total + new_rating_score
+            print("ratings total")
+            print(retrieve_product.rating_total)
+
+            # Adding 1 to the amount of reviews under the product
+            retrieve_product.number_of_ratings = number_of_ratings + 1
+            print("How many reviews there are")
+            print(retrieve_product.number_of_ratings)
+
+            # Finding the average of the reviews
+            retrieve_product.rating = retrieve_product.rating_total / retrieve_product.number_of_ratings
+            print("Average rating")
+            print(retrieve_product.rating)
+
+            retrieve_product.save()
+            
             messages.success(request, 'Review added.')
             print("Review added.")
             return redirect(reverse('products'))
