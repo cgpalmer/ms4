4b0c7dad4c286d842a21f52bbd8b61d53cb7c8b0
Chris Palmer <cgpalmer91@gmail.com>
2020-11-17 20:05:25 +0000

    Add to basket function: when a user adds an item they already have and it matches the delivery style, then it will increase the quantity. Also changed the name of the test json to basket.

--- basket/views.py
+++ basket/views.py
@@ -15,14 +15,14 @@ def add_to_basket(request, item_id):
     print("Item ID follows")
     print(item_id)
     print(type(item_id))
-    test = request.session.get('test', {})
+    basket = request.session.get('basket', {})
     print("this is the test from the bag so far")
-    print(test)
-    if test != {}:
+    print(basket)
+    if basket != {}:
         print("bag exists")
         matching_items = []
 
-        for item in test['items']:
+        for item in basket['items']:
             print(item)
             if item_id == item['item_id'] and digital_download == item['digital_download']:
                 print("We have a match")
@@ -30,58 +30,24 @@ def add_to_basket(request, item_id):
         if matching_items:
             print("matching items exist")
             if matching_items[0]['item_id'] == item_id:
-                test['items'].append({
-                        'item_id': item_id,
-                        'digital_download': digital_download,
-                        'quantity': quantity
-                })
+                matching_items[0]['quantity'] += quantity
         else:
             print("bag exists but no matching item")
-            test['items'].append({
+            basket['items'].append({
                         'item_id': item_id,
                         'digital_download': digital_download,
                         'quantity': quantity
                 })
     else:
         print("bag doesnt exist yet.")
-        test['items'] = []
-        test['items'].append({
+        basket['items'] = []
+        basket['items'].append({
                     'item_id': item_id,
                     'digital_download': digital_download,
                     'quantity': quantity
             })
             
-
-    # print(test)
-    # quantity = int(request.POST.get('quantity'))
-    # digital_download = None
-    # print("quantity")
-    # print(quantity)
-    # if 'digital_download' in request.POST:
-    #     digital_download = request.POST.get('digital_download')
-    #     print(digital_download)
     redirect_url = request.POST.get('redirect_url')
-    # basket = request.session.get('basket', {})
-  
-    # if digital_download:
-      
-    
-    #     if item_id in list(basket.keys()):
-    #         if digital_download in basket[item_id]['items_by_digital_download'].keys():
-    #             basket[item_id]['items_by_digital_download'][digital_download] += quantity
-    #         else:
-    #             basket[item_id]['items_by_digital_download'][digital_download] = quantity
-    #     else:
-    #         digital_download[item_id] = {'items_by_digital_download': {digital_download: quantity}}
-    # else:
-    #     if item_id in list(basket.keys()):
-    #         basket[item_id] += quantity
-    #     else:
-    #         basket[item_id] = quantity
-
-    
-
-    request.session['test'] = test
-    # request.session['data'] = data
-    # print(basket)
+    request.session['basket'] = basket
+    print(basket)
     return redirect(redirect_url)
