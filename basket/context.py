dded4a678a12251b2b85cf9e412f9f1cfe81d3ed
Chris Palmer <cgpalmer91@gmail.com>
2020-11-17 23:42:57 +0000

    Recreating the context to pass through the product, basket item quantities, basket items and the subtotals. Altered the html to display the correct values too.

--- basket/context.py
+++ basket/context.py
@@ -9,28 +9,50 @@ def basket_contents(request):
     total = 0
     product_count = 0
     basket = request.session.get('basket', {})
+    print(basket)
+    print("this is the context page")
+    print(basket['items'])
+    for item in basket['items']:
+        print(item)
+        print(item['item_id'])
+        product = get_object_or_404(Product, pk=item['item_id'])
+        print(product)
+        print("product price")
+        print(product.price)
+        print(type(product.price))
+        print(item['quantity'])
+        print(type(item['quantity']))
 
-    for item_id, item_data in basket.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            basket_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-                })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for digital_download, quantity in item_data['items_by_download_choice'].items():
-                total += quantity * product.price
-                product_count += quantity
-                basket_items.append({
-                    'item_id': item_id,
-                    'quantity': item_data,
-                    'product': product,
-                    'digital_download': digital_download
-                })
+        subtotal = 4 * 3
+
+        basket_items.append({
+            'item': item,
+            'product': product,
+            'subtotal': subtotal
+        })
+    print("basket_items")
+    print(basket_items)
+    # for item_id, item_data in basket.items():
+    #     if isinstance(item_data, int):
+    #         product = get_object_or_404(Product, pk=item_id)
+    #         total += item_data * product.price
+    #         product_count += item_data
+    #         basket_items.append({
+    #             'item_id': item_id,
+    #             'quantity': item_data,
+    #             'product': product,
+    #             })
+    #     else:
+    #         product = get_object_or_404(Product, pk=item_id)
+    #         for digital_download, quantity in item_data['items_by_download_choice'].items():
+    #             total += quantity * product.price
+    #             product_count += quantity
+    #             basket_items.append({
+    #                 'item_id': item_id,
+    #                 'quantity': item_data,
+    #                 'product': product,
+
+    #             })
 
     if total < settings.FREE_DELIVERY_AMOUNT:
         delivery = Decimal.from_float(settings.STANDARD_DELIVERY_AMOUNT)
@@ -42,14 +64,14 @@ def basket_contents(request):
     grand_total = delivery + total
     
     context = {
-        'basket_items': basket_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_deficit': free_delivery_deficit,
-        'FREE_DELIVERY_AMOUNT': settings.FREE_DELIVERY_AMOUNT,
-        'grand_total': grand_total,
-        'new_customer_offer': settings.NEW_CUSTOMER_OFFER,
+        'basket_items': basket_items
+        # 'total': total,
+        # 'product_count': product_count,
+        # 'delivery': delivery,
+        # 'free_delivery_deficit': free_delivery_deficit,
+        # 'FREE_DELIVERY_AMOUNT': settings.FREE_DELIVERY_AMOUNT,
+        # 'grand_total': grand_total,
+        # 'new_customer_offer': settings.NEW_CUSTOMER_OFFER,
       
     }
 
