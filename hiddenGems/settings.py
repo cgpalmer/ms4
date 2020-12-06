5acf641e938c7cb1484cd11681c3a9b518a6fbd1
Chris Palmer <cgpalmer91@gmail.com>
2020-12-06 18:17:32 +0000

    Included a tox.ini file to control flake 8 and changed login_redirect_url to just '/' so it redirects to the home page when someone logs in successfully.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -113,7 +113,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = False
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/loginSuccess'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'hiddenGems.wsgi.application'
 
