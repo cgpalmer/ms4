d06c15051dd6bbaf03b47ec765aff08c98bb09f3
Chris Palmer <cgpalmer91@gmail.com>
2020-12-14 17:08:59 +0000

    Trying to fix heroku bugs - turning off debug to see if it's a factor.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hidden-gems-cgpalmer91.herokuapp.com', 'localhost']
 
