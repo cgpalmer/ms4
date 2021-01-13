946fe96ace71ba8950c2d717c26731d4b2798a55
Chris Palmer <cgpalmer91@gmail.com>
2021-01-13 18:11:33 +0000

    Enabling the user to search by sku.

--- products/views.py
+++ products/views.py
@@ -77,7 +77,7 @@ def all_products(request):
 
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query) | Q(
-                friendly_name__icontains=query)
+                friendly_name__icontains=query) | Q(sku__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
