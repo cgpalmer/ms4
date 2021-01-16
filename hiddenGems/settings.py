02dfb34c5b0d1bbbf7377b7fd53520c7e4b201b0
Chris Palmer <cgpalmer91@gmail.com>
2021-01-16 16:32:33 +0000

    Removing locally held images as they are mounted on a separate server and reloading all the products back onto heroku server for deployment.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -122,27 +122,21 @@ WSGI_APPLICATION = 'hiddenGems.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
--- products/models.py
+++ products/models.py
@@ -47,7 +47,7 @@ class Product(models.Model):
 
 
 class Special(models.Model):
-    name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254, null=False, blank=True, default="none")
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     discounts = models.DecimalField(max_digits=4, decimal_places=3, default=0.0, null=False, blank=False)
 
