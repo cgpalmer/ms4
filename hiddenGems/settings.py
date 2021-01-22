78965ba139eef4507b8fe34ef58f8f3eae45f8bf
Chris Palmer <cgpalmer91@gmail.com>
2021-01-22 00:56:55 +0000

    Adding in the images as image mobile unless they're being downloaded or as an extrnal link.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
