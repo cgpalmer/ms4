5c704c13d47ecde44ae80fceaaf127492cdb718c
Chris Palmer <cgpalmer91@gmail.com>
2021-01-27 17:40:58 +0000

    Removing the null from name on special products.

--- products/models.py
+++ products/models.py
@@ -38,8 +38,8 @@ class Product(models.Model):
     # check these as things will break if this is allowed to be null.
     number_of_pictures = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 
 class Special(models.Model):
@@ -47,11 +47,9 @@ class Special(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     discounts = models.DecimalField(max_digits=4, decimal_places=3, default=0.0, null=True, blank=False)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 class Image_upload(models.Model):
