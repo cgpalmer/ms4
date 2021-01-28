daa625e4d47fd15f9075d83240fdd73fb8ecdcc8
Chris Palmer <cgpalmer91@gmail.com>
2021-01-28 14:09:17 +0000

    Writing a testing script for the review form.

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Review(models.Model):
     product = models.ForeignKey('products.Product', null=True, blank=True, on_delete=models.SET_NULL)
-    review_rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
+    review_rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)],
                                         null=False, blank=False)
     user = models.CharField(max_length=254, null=False, blank=False, default="User not found")
     review_content = models.TextField(default="")
--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import ReviewForm
+
+class TestReviewForm(TestCase):
+    def test_review_rating_is_required(self):
+        form = ReviewForm({'review_rating': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('review_rating', form.errors.keys())
+        self.assertEqual(form.errors['review_rating'][0], 'This field is required.')
+
+    def test_review_content_is_required(self):
+        form = ReviewForm({'review_content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('review_content', form.errors.keys())
+        self.assertEqual(form.errors['review_content'][0], 'This field is required.')
+
+    def test_review_fiels_are_accurate(self):
+        form = ReviewForm()
+        self.assertNotEquals(form.Meta.exclude, ['user', 'product'])  
+
+    def test_review_rating_is_maxed_at_5(self):
+        form = ReviewForm({'review_rating': 6})
+        self.assertFalse(form.is_valid())
+        self.assertIn('review_rating', form.errors.keys())
+        self.assertEqual(form.errors['review_rating'][0], 'Ensure this value is less than or equal to 5.')
+
+    def test_review_rating_not_below_0(self):
+        form = ReviewForm({'review_rating': -1})
+        self.assertFalse(form.is_valid())
+        self.assertIn('review_rating', form.errors.keys())
+        self.assertEqual(form.errors['review_rating'][0], 'Ensure this value is greater than or equal to 0.')
