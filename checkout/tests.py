8f78cf27b3d50012f4c01966261a91fbca9b7752
Chris Palmer <cgpalmer91@gmail.com>
2021-01-28 15:55:09 +0000

    Trialling different settings for the testing.

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,29 +1,4 @@
 from django.test import TestCase
 
-class TestUrls(SimpleTestCase):
 
-    def test_view_basket_url_resolves(self):
-        url = reverse('view_basket')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, view_basket)
-
-    def test_add_to_basket_url_resolves(self):
-        url = reverse('add_to_basket', args=['item_id'])
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, add_to_basket)
-
-    def test_edit_basket_url_resolves(self):
-        url = reverse('edit_basket')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, edit_basket)
-
-    def test_delete_basket_item_url_resolves(self):
-        url = reverse('delete_basket_item', args=['item_id'])
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, delete_basket_item)
-
-    def test_empty_basket_url_resolves(self):
-        url = reverse('empty_basket')
-        print(resolve(url))
-        self.assertEquals(resolve(url).func, empty_basket)
 
