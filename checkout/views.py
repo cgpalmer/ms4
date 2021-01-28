f1369aa8a3c6021165b26eb9e94c04d477b7e19b
Chris Palmer <cgpalmer91@gmail.com>
2021-01-28 10:37:08 +0000

    Putting in a fail safe on the condensing basket function to stop users from placing the same order twice. It also stops the site from breaking if they try to process an empty basket.

--- checkout/views.py
+++ checkout/views.py
@@ -34,42 +34,46 @@ def cache_checkout_data(request):
 
 def condensing_basket(request):
     basket = request.session.get('basket', {})
-    list_of_items_to_check = []
-    duplicates_found = []
-    for item in basket['items']:
-
-        appending_item = [item['basket_item_id'], int(item['quantity']), [item['item_id'], item['digital_download'],
-                          item['linked_products']]]
-        list_of_items_to_check.append(appending_item)
-
-    for i in range(0, len(list_of_items_to_check)):
-        for j in range(i+1, len(list_of_items_to_check)):
-            if list_of_items_to_check[i][2] == list_of_items_to_check[j][2]:
-                print("duplicate found")
-                print(list_of_items_to_check[j])
-                print(list_of_items_to_check[i])
-                duplicates_found.append([list_of_items_to_check[i], list_of_items_to_check[j]])
-                print(duplicates_found)
-
-    for k in range(len(duplicates_found)):
+    if basket != {}:
+        list_of_items_to_check = []
+        duplicates_found = []
         for item in basket['items']:
-            if item['digital_download'] is None:
-                if item['basket_item_id'] == duplicates_found[k][0][0]:
-                    item['quantity'] = item['quantity'] + duplicates_found[k][1][1]
-                    request.session['basket'] = basket
-                    print("new item quantity")
-                    print(item['quantity'])
 
-    for k in range(len(duplicates_found)):
-        print(len(duplicates_found))
-        item_number = -1
-        for item in basket['items']:
-            item_number = item_number + 1
-            print(item_number)
-            if item['basket_item_id'] == duplicates_found[k][1][0]:
-                del basket['items'][item_number]
-            request.session['basket'] = basket
-    return redirect(checkout)
+            appending_item = [item['basket_item_id'], int(item['quantity']), [item['item_id'], item['digital_download'],
+                            item['linked_products']]]
+            list_of_items_to_check.append(appending_item)
+
+        for i in range(0, len(list_of_items_to_check)):
+            for j in range(i+1, len(list_of_items_to_check)):
+                if list_of_items_to_check[i][2] == list_of_items_to_check[j][2]:
+                    print("duplicate found")
+                    print(list_of_items_to_check[j])
+                    print(list_of_items_to_check[i])
+                    duplicates_found.append([list_of_items_to_check[i], list_of_items_to_check[j]])
+                    print(duplicates_found)
+
+        for k in range(len(duplicates_found)):
+            for item in basket['items']:
+                if item['digital_download'] is None:
+                    if item['basket_item_id'] == duplicates_found[k][0][0]:
+                        item['quantity'] = item['quantity'] + duplicates_found[k][1][1]
+                        request.session['basket'] = basket
+                        print("new item quantity")
+                        print(item['quantity'])
+
+        for k in range(len(duplicates_found)):
+            print(len(duplicates_found))
+            item_number = -1
+            for item in basket['items']:
+                item_number = item_number + 1
+                print(item_number)
+                if item['basket_item_id'] == duplicates_found[k][1][0]:
+                    del basket['items'][item_number]
+                request.session['basket'] = basket
+        return redirect(checkout)
+    else:
+        messages.error(request, 'It looks like your basket is empty. Please add some items to continue.')
+        return redirect('view_basket')
 
 
 def checkout(request):
