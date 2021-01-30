2928c3de8a20ce1f5d8b7c42c09d75d575d47951
Chris Palmer <cgpalmer91@gmail.com>
2021-01-30 04:00:24 +0000

    Fixing a bug where the loops for products was 'g' and was changed but I left an accident;y g int here still.

--- hiddenGems/settings.py
+++ hiddenGems/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hidden-gems-cgpalmer91.herokuapp.com', 'localhost']
 
